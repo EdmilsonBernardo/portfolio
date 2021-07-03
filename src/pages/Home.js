b51ea8fdaeecaf86fc6bd8709dcfd3e674158332
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-07-03 18:38:21 -0300

    Add text of the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import imgProfile from '../images/profile.jpg';
 
 class Home extends React.Component {
   render() {
     return (
       <>
+        <section
+          style={{
+            display: 'flex',
+            justifyContent: 'space-evenly',
+            flexWrap: 'wrap',
+            alignItems: 'center',
+            height: '80vh',
+            margin: '25px',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              alignItems: 'center',
+              width: '500px',
+            }}
+          >
+            <p>Hello, welcome to my portfolio! Here you will find information about me and the projects I have developed. Please feel free to get in touch with me, I will be happy to receive your feedback or help you in any way.</p>
+          </div>
+          <img
+            src={ imgProfile }
+            alt="edmilson bernardo"
+            style={{
+              width: '300px',
+              borderRadius: '30px'
+            }}
+          />
+        </section>
       </>
     );
   } 
